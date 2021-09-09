62361af8a12be06fc2d9826f5e514ad2646eec80
noboard <noboard@gmail.com>
2021-09-09 18:23:10 +0100

    modifying config to change depending on environment

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,3 +1,5 @@
+use std::convert::{TryFrom, TryInto};
+
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
@@ -21,8 +23,23 @@ pub struct ApplicationSettings {
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let mut settings = config::Config::default();
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("configuration");
 
-    settings.merge(config::File::with_name("configuration"))?;
+    //Read the default configuration file
+    settings.merge(config::File::from(configuration_directory.join("base")).required(true))?;
+
+    //Detect the running environment
+    //Default to 'local' if unspecified.
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT.");
+
+    //Layer on the environment-specifi values
+    settings.merge(
+        config::File::from(configuration_directory.join(environment.as_str())).required(true);
+    )?;
     settings.try_into()
 }
 
--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ async fn main() -> std::io::Result<()> {
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to postgres database");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address)
         .expect("main() failed to bind to random port!");
 
